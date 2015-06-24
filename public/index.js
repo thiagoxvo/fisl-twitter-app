b27a1839d60d5e5d10670b3163e817260c2ae612
Thiago Xavier <thiagoxvo@gmail.com>
2015-06-24 00:35:22 -0300

    Build a pixel matrix dinamically

--- public/index.js
+++ public/index.js
@@ -1,4 +1,20 @@
-var matrixSize = 9;
+var matrixSize = 50; //3x3
+var pixelSize = 50; //50px
+
+var buildMatrix = function() {
+  var tag = "";
+  var $container = $('.pixel-container');
+  for( var i = 0; i < matrixSize; i++) {
+    var left = "left:" + (i * pixelSize) + "px;";
+    // tag = '<div class="pixel" style="'+ left +'""></div>';
+    for( var j = 0; j < matrixSize; j++) {
+      var top = "top:" + (j * pixelSize) + "px;";
+      tag = '<div class="pixel" style="'+ top + left +'""></div>';
+      $container.append(tag);   
+    }
+  }  
+};
+buildMatrix();
 
 var buildImageTag = function(){
   var timestamp = Date.now();
@@ -14,7 +30,7 @@ var getLoadedImage = function(){
   return $('.preload img').detach();
 }
 
-setInterval(function(){
+// setInterval(function(){
   var position = Math.floor(Math.random() * matrixSize) + 1
   console.log(position);
   var img = getLoadedImage();
@@ -25,4 +41,4 @@ setInterval(function(){
   $('td[data-position="'+ position +'"]')
     .empty()
     .html(img);
-},1000);
+// },1000);
