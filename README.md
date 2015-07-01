4591ba1cb4d42efbd56bc6d9f7e184e8ed2e7272
Thiago Xavier <thiagoxvo@gmail.com>
2015-07-01 20:11:48 -0300

    Get the hashtag from command line

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# fisl-twitter-spike
+# FISL16 - ThoughtWorks 
+
+Application built to show profile images from tweets with a specific hashtag in a pixelated world map, it was used on FISL16 Thoughtworks booth.
 
 Before install, export Twitter app credentials:
-
 ```
 export CONSUMER_KEY=""
 export CONSUMER_SECRET=""
@@ -9,10 +10,13 @@ export TOKEN=""
 export TOKEN_SECRET=""
 ```
 
-To run the server 
-
+To install dependencies:
 ```
 npm install
-node server
 ```
 
+To run the server:
+```
+node server #hashtag
+```
+ If no one hashtag is provided will use #fisl.
--- twitter-client.js
+++ twitter-client.js
@@ -6,7 +6,7 @@ var config = {
   accessToken: process.env.TOKEN || 'token',
   accessTokenSecret: process.env.TOKEN_SECRET || 'token_secret'
 }
-
+var hashtag = process.argv[2] || '#fisl';
 var twitter = new Twitter(config);
 
 var error = function (err, response, body) {
@@ -17,8 +17,8 @@ var getMiniImage = function(imageName) {
   return imageName.replace("_normal", "_mini");
 }
 
-var searchHashtag = function(callback){
-  twitter.getSearch({'q':'#copaamerica2015','count': 100}, error, function(data){
+var searchHashtag = function(callback) {
+  twitter.getSearch({'q': hashtag,'count': 100}, error, function(data){
     var tweets = JSON.parse(data)["statuses"];
     var images = tweets.map(function(item) {
       return getMiniImage(item["user"].profile_image_url); 
